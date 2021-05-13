8667b0d0e8853cbf3dc15826aa27cb0788211dfe
Echo-chy <2838781746@qq.com>
2021-05-13 21:28:16 +0800

    第二次版本提交

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'App',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+	@import "./assets/css/base.css"
+</style>
